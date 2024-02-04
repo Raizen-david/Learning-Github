55b6312d8b3d8692bcfc00f38cb3df527257228e
grey <davido9634@gmail.com>
2024-02-04 14:33:40 +0000

    Initial Commit

---
+++ learn-git/learn_gitApp.swift
@@ -0,0 +1,17 @@
+//
+//  learn_gitApp.swift
+//  learn-git
+//
+//  Created by David Adekunle on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct learn_gitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
